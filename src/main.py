2181cef87d82bb064890a9f4a1a0612347d3ec36
Salmontree <brexiosaurus@gmail.com>
2025-07-25 17:25:46 -0400

    first commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+class Project:
+    def __init__(self) -> None:
+        self.name = "Ascent Project"
+        self.data_format = "80" # 1.21.7
+        self.res_format = "60"  # 1.21.7
+
+        self.input_path = ""
+        self.output_path = "../" + self.name
