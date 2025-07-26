3a5bf64ecf1ca1944acbe759499aaa0e6339b2f8
Salmontree <brexiosaurus@gmail.com>
2025-07-26 03:24:55 -0400

    COMPLETELY RESET EVERYTHING

--- src/create.py
+++
@@ -1,7 +0,0 @@
-# TODO: Generates the datapack structure (folders) then pastes in all the files from the parse process
-
-# Script to convert to kebab-case, will need this for later
-# '-'.join(sub(r"(\s|_|-)+"," ", sub(r"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+", lambda mo: ' ' + mo.group(0).lower(), project.name)).split())
-
-def create(project):
-    pass
